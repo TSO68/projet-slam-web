7a0bf65e0d3215e7836c2f10083713f3f0ed720f
vic1997 <vic-du-68@live.fr>
2017-05-02 22:37:49 +0200

    Ajout de l'héritage + classe Personnel et Staff
    - Ajout classe Personnel.
    - Ajout classe Staff.
    - Héritage de la classe Personnel vers Joueur
    - Problème d'héritage (erreurs) à régler

--- Client/Client/Joueur.cs
+++ Client/Client/Joueur.cs
@@ -6,126 +6,24 @@ using System.Threading.Tasks;
 
 namespace Client
 {
-    class Joueur
+    class Joueur : Personnel
     {
-        private int id;
-        private String nom;
-        private String prenom;
-        private DateTime dateNaiss;
-        private String lieuNaiss;
-        private int age;
-        private String biographie;
         private int num;
         private float taille;
         private float poids;
         private String pied;
+        private int agee;
         private DateTime dateVenueClub;
+        
 
-        public Joueur(int id, string nom, string prenom, DateTime dateNaiss, string lieuNaiss, int age, string biographie, int num, float taille, float poids, string pied, DateTime dateVenueClub)
+        public Joueur(int num, float taille, float poids, string pied, DateTime dateVenueClub) : base(num, taille, poids, pied,dateVenueClub, age)
         {
-            this.id = id;
-            this.nom = nom;
-            this.prenom = prenom;
-            this.dateNaiss = dateNaiss;
-            this.lieuNaiss = lieuNaiss;
-            this.age = age;
-            this.biographie = biographie;
             this.num = num;
             this.taille = taille;
             this.poids = poids;
             this.pied = pied;
             this.dateVenueClub = dateVenueClub;
-        }
-
-        public int Id
-        {
-            get
-            {
-                return id;
-            }
-
-            set
-            {
-                id = value;
-            }
-        }
-
-        public string Nom
-        {
-            get
-            {
-                return nom;
-            }
-
-            set
-            {
-                nom = value;
-            }
-        }
-
-        public string Prenom
-        {
-            get
-            {
-                return prenom;
-            }
-
-            set
-            {
-                prenom = value;
-            }
-        }
-
-        public DateTime DateNaiss
-        {
-            get
-            {
-                return dateNaiss;
-            }
-
-            set
-            {
-                dateNaiss = value;
-            }
-        }
-
-        public string LieuNaiss
-        {
-            get
-            {
-                return lieuNaiss;
-            }
-
-            set
-            {
-                lieuNaiss = value;
-            }
-        }
-
-        public int Age
-        {
-            get
-            {
-                return age;
-            }
-
-            set
-            {
-                age = value;
-            }
-        }
-
-        public string Biographie
-        {
-            get
-            {
-                return biographie;
-            }
-
-            set
-            {
-                biographie = value;
-            }
+            
         }
 
         public int Num
@@ -192,7 +90,5 @@ namespace Client
                 dateVenueClub = value;
             }
         }
-        
-
     }
 }
---
+++ Client/Client/Personnel.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Personnel
+    {
+        private int id;
+        private String nom;
+        private String prenom;
+        private DateTime dateNaiss;
+        private String lieuNaiss;
+        private int age;
+        private String biographie;
+
+        public Personnel(int id, string nom, string prenom, DateTime dateNaiss, string lieuNaiss, int age, string biographie)
+        {
+            this.id = id;
+            this.nom = nom;
+            this.prenom = prenom;
+            this.dateNaiss = dateNaiss;
+            this.lieuNaiss = lieuNaiss;
+            this.age = age;
+            this.biographie = biographie;
+        }
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Nom
+        {
+            get
+            {
+                return nom;
+            }
+
+            set
+            {
+                nom = value;
+            }
+        }
+
+        public string Prenom
+        {
+            get
+            {
+                return prenom;
+            }
+
+            set
+            {
+                prenom = value;
+            }
+        }
+
+        public DateTime DateNaiss
+        {
+            get
+            {
+                return dateNaiss;
+            }
+
+            set
+            {
+                dateNaiss = value;
+            }
+        }
+
+        public string LieuNaiss
+        {
+            get
+            {
+                return lieuNaiss;
+            }
+
+            set
+            {
+                lieuNaiss = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+
+            set
+            {
+                age = value;
+            }
+        }
+
+        public string Biographie
+        {
+            get
+            {
+                return biographie;
+            }
+
+            set
+            {
+                biographie = value;
+            }
+        }
+    }
+}
