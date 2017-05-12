5a09c3ba9611685217244db10242c96b312193e5
Unknown <thespecialone68@hotmail.fr>
2017-05-12 21:04:13 +0200

    Ajout Insert joue + joueur
    Et suppression readme+licence

---
+++ BDD/insert_joueur.sql
@@ -0,0 +1,32 @@
+INSERT INTO JOUEUR (num, taille, poids, pied, venueClub, id) 
+VALUES
+-- Gardiens
+(1,1.84,72,"droit","Au club depuis juillet 2016",5),
+(16,1.85,78,"droit","Au club depuis août 2014",6),
+(30,1.83,75,"droit","Formé au club",7),
+(40,1.84,79,"droit","Au club depuis juillet 2016",8),
+-- Défenseurs
+(3,1.86,70,"gauche","Au club depuis juillet 2014",9),
+(12,1.93,90,"droit","Au club depuis août 2016",10),
+(21,1.88,85,"droit","Au club depuis juillet 2013",11),
+(22,1.85,84,"droit","Au club depuis juillet 2014",12),
+(23,1.87,83,"gauche","Au club depuis juillet 2015",13),
+(24,1.72,67,"droit","Au club depuis juillet 2015",14),
+-- Milieux
+(5,1.70,61,"droit","Au club depuis juillet 2016",15),
+(6,1.81,69,"droit","Au club depuis juillet 2013",16),
+(7,1.83,70,"droit","Formé au club",17),
+(8,1.86,71,"gauche","Au club depuis janvier 2017",18),
+(17,1.78,79,"droit","Au club depuis juillet 2016",19),
+(18,1.76,67,"droit","Au club depuis octobre 2014",20),
+(20,1.77,70,"droit","Au club depuis juillet 2016",21),
+(33,1.84,70,"droit","Formé au club",22),
+(34,1.85,79,"droit","Formé au club",23),
+(35,1.75,67,"droit","Formé au club",24),
+-- Attaquants
+(9,1.89,77,"droit","Au club depuis juillet 2016",25),
+(11,1.84,77,"gauche","Au club depuis juillet 2013",26),
+(13,1.87,80,"droit","Au club depuis janvier 2015",27),
+(19,1.85,78,"droit","Au club depuis juillet 2014",28),
+(28,1.84,75,"droit","Au club depuis juillet 2016",29),
+(29,1.90,80,"droit","Au club depuis juillet 2016",30);
