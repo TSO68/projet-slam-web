04efa7601b162b7fd974e961f23cfc4b628e82f3
Unknown <riti97@hotmail.fr>
2017-05-24 23:04:36 +0200

    footer corrigé

--- footer.php
+++ footer.php
@@ -1,38 +1,38 @@
 <br>
 <footer>
 	<center>
-		<div class="contour">
-		<div style="overflow-x:auto;">
-			<table>
-				<tr class="head">
-					<td width='30%'>
-						<div style="margin-left : 5%;">
-							Site réalisé dans le cadre des projets à présenter en deuxième année de BTS SIO
-							<br/>
-							<strong>HAUSHERR Victor, KAMMERER Timothée, KANAWATI Samy</strong>
-						</div>
-					</td>
-						
-					<td width='40%'>
-					<!--Redirection vers le haut de la page-->
-					<center><a href="#" title="Haut de page"><img src ='Images/retour_haut_page.png' width='150px'/></a></center>
+		<div class="liste">
+			<div style="overflow-x:auto;">
+				<table style="color:#fff">
+					<tr>
+						<td width='30%'>
+							<div style="margin-left : 5%;">
+								Site réalisé dans le cadre des projets à présenter en deuxième année de BTS SIO
+								<br/>
+								<strong>HAUSHERR Victor, KAMMERER Timothée, KANAWATI Samy</strong>
+							</div>
+						</td>
 							
-					</td>
-						
-					<td width='30%'>
-						<!--Localisation de la société-->
-						<br>
-						<address style="margin-left : 45%; margin-right : 5%">
-							FC SIO<br/>
-							42 avenue de l'Europe<br/>
-							68000 Colmar Cedex<br/>
-						</address>
-						<br/>
-					</td>
-				</tr>
-			</table> 
+						<td width='40%'>
+						<!--Redirection vers le haut de la page-->
+						<center><a href="#" title="Haut de page"><img src ='Images/retour_haut_page.png' width='150px'/></a></center>
+								
+						</td>
+							
+						<td width='30%'>
+							<!--Localisation de la société-->
+							<br>
+							<address style="margin-left : 45%; margin-right : 5%">
+								FC SIO<br/>
+								42 avenue de l'Europe<br/>
+								68000 Colmar Cedex<br/>
+							</address>
+							<br/>
+						</td>
+					</tr>
+				</table> 
+			</div>
 		</div>
-	</div>
 	</center>
 </footer>
 
